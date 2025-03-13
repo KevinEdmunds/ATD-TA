449c3fb6df21561b6d16d277445490837e8780eb
Kevin Edmunds <kevin.edmunds@entelect.co.za>
2025-03-13 14:36:16 +0200

    adding, modifying and deleting items works

--- OrderBook.java
+++ OrderBook.java
@@ -1,97 +1,146 @@
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDateTime;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Objects;
 
 public class OrderBook {
     private static LinkedList<OrderPrice> buyOrderBook = new LinkedList<OrderPrice>();
     private LinkedList<OrderPrice> sellOrderBook = new LinkedList<OrderPrice>();
-//    private static Iterator buyIterator =  buyOrderBook.iterator();
-//    private Iterator sellIterator =  sellOrderBook.iterator();
+    public static int orderId=0;
 
     public static void main(String[] args) throws IOException {
-        // Enter data using BufferReader
-        int i=0;
         while(true)
         {
             BufferedReader r = new BufferedReader(
                     new InputStreamReader(System.in));
 
             System.out.println("< - - - - - - - - - - - - - >");
-            System.out.print("Enter Price: ");
-            float price = Float.parseFloat(r.readLine());
+            System.out.print("Select operation: ");
+            String operation = r.readLine();
 
-            System.out.print("Enter Qty: ");
-            int quantity = Integer.parseInt(r.readLine());
-
-            System.out.print("Enter Side: ");
-            String side = r.readLine();
-
-            Order newOrder = new Order(i, price, quantity,side, LocalDateTime.now());
-            Add(newOrder);
-            i++;
-            PrintOrderList();
+            switch (operation) {
+                case "Add" -> AddOrderDetails();
+                case "Delete" -> DeleteOrderDetails();
+                case "Modify" -> ModifyOrderDetails();
+                case null, default -> System.out.println("Invalid operation");
+            }
         }
     }
 
-    public void Delete(int id) {
+    private static void ModifyOrderDetails() throws IOException{
+        BufferedReader r = new BufferedReader(
+                new InputStreamReader(System.in));
 
+        System.out.print("Order ID: ");
+        int id = Integer.parseInt(r.readLine());
+
+        System.out.print("Quantity change: ");
+        int quantity = Integer.parseInt(r.readLine());
+
+        Modify(id, quantity);
+    }
+
+
+    private static void DeleteOrderDetails() throws IOException {
+        BufferedReader r = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        int id = Integer.parseInt(r.readLine());
+
+        Delete(id);
+    }
+
+    private static void AddOrderDetails() throws IOException {
+        BufferedReader r = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.println("< - - - - - - - - - - - - - >");
+        System.out.print("Enter Price: ");
+        float price = Float.parseFloat(r.readLine());
+
+        System.out.print("Enter Qty: ");
+        int quantity = Integer.parseInt(r.readLine());
+
+        System.out.print("Enter Side: ");
+        String side = r.readLine();
+
+        Order newOrder = new Order(orderId, price, quantity,side, LocalDateTime.now());
+        Add(newOrder);
+        orderId++;
+        PrintOrderList();
+    }
+
+    public static void Modify(int id, int quantity){
+        int index = 0;
+        for(OrderPrice orderPrice: buyOrderBook){
+            index = orderPrice.FindOrderByID(id);
+            if(index!=-1)
+            {
+                orderPrice.ModifyOrder(index, quantity);
+                break;
+            }
+        }
+        PrintOrderList();
+    }
+
+    public static void Delete(int id) {
+        int index = 0;
+        for(OrderPrice orderPrice: buyOrderBook){
+           index = orderPrice.FindOrderByID(id);
+            if(index!=-1)
+            {
+                orderPrice.DeleteOrder(index);
+                break;
+            }
+        }
+        PrintOrderList();
+        System.out.println("This is the item to delete: " +index );
     }
 
     public static void Add(Order newOrder){
         float price = newOrder.orderPrice;
-        System.out.println("Adding item");
-//        if(buyOrderBook.isEmpty())
-//        {
-//            System.out.println("Initializing Order Book");
-//            OrderPrice newOrderPrice= new OrderPrice();
-//            newOrderPrice.AddOrder(newOrder);
-//        }
+        //System.out.println("Adding item");
 
         if(Objects.equals(newOrder.orderSide, "Buy")) {
-            System.out.println("Buy order");
+            //System.out.println("Buy order");
             if(buyOrderBook.isEmpty())
             {
                 buyOrderBook.add(CreateNewOrderPrice(newOrder));
-                System.out.println("This is a new order");
+               //System.out.println("This is a new order");
                 return;
             }
             if(price>buyOrderBook.getLast().orderPrice)
             {
                 buyOrderBook.addLast(CreateNewOrderPrice(newOrder));
-                System.out.println("The new biggest entry");
+                //System.out.println("The new biggest entry");
                 return;
             }else if(price<buyOrderBook.getFirst().orderPrice)
             {
                 buyOrderBook.addFirst(CreateNewOrderPrice(newOrder));
-                System.out.println("The new smallest entry");
+                //System.out.println("The new smallest entry");
                 return;
             }else{
-                System.out.println("Running in the for loop");
+                //System.out.println("Running in the for loop");
                 for(int i=0; i<buyOrderBook.size();i++)
                 {
 
                     float currentOrderPrice = buyOrderBook.get(i).orderPrice;
-                    System.out.println("Current price :" + currentOrderPrice +"Expected price: " + price );
+                    //System.out.println("Current price :" + currentOrderPrice +"Expected price: " + price );
                     if(price==currentOrderPrice){
                         buyOrderBook.get(i).AddOrder(newOrder);
-                        System.out.println("Order added to an existing table");
+                        //System.out.println("Order added to an existing table");
                         return;
                     }
                     else if(price<currentOrderPrice){
                         buyOrderBook.add(i, CreateNewOrderPrice(newOrder));
-                        System.out.println("No entry for this value, creating new entry");
+                        //System.out.println("No entry for this value, creating new entry");
                         return;
                     }
                 }
                 buyOrderBook.addLast(CreateNewOrderPrice(newOrder));
-            }
-
-            //System.out.println("This is a buy order");
+            }//System.out.println("This is a buy order");
         }else if(Objects.equals(newOrder.orderSide, "Sell")){
             System.out.println("This is a sell order");
         }else{
@@ -114,9 +163,5 @@ public class OrderBook {
             orderLine.PrintOrderPriceList();
             System.out.println();
         }
-    }
-
-    public void Modify(){
-
     }
 }
--- OrderPrice.java
+++ OrderPrice.java
@@ -1,14 +1,28 @@
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class OrderPrice {
-    public Queue<Order> orderPriceList = new LinkedList<Order>();
+    public LinkedList<Order> orderPriceList = new LinkedList<Order>();
     public float orderPrice;
 
     //queue order based on priority (date/time submitted)
     public void AddOrder(Order newOrder) {
-        System.out.println("Adding an item to an existing price line");
+        //System.out.println("Adding an item to an existing price line");
         orderPriceList.add(newOrder);
+    }
+    public void DeleteOrder(int id)
+    {
+        orderPriceList.remove(id);
+    }
+    public void ModifyOrder(int id, int quanitiy)
+    {
+        Order modifiedOrder = orderPriceList.get(id);
+        modifiedOrder.orderQuantity=quanitiy;
+        modifiedOrder.orderDate= LocalDateTime.now();
+
+        orderPriceList.remove(id);
+        orderPriceList.addLast(modifiedOrder);
     }
     public void SetOrderPrice(float price)
     {
@@ -18,7 +32,17 @@ public class OrderPrice {
         //System.out.println(orderPriceList.size());
         for(Order orders: orderPriceList)
         {
-            System.out.print(orders.orderQuantity + " - - > ");
+            System.out.print(orders.orderQuantity + " (" + orders.orderId+") "+ " - - > ");
         }
+    }
+    public Integer FindOrderByID(Integer id){
+        int index =0;
+        for(Order order: orderPriceList){
+            if(order.orderId==id){
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 }
